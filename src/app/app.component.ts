3ff23769c6d6d45e266470616a488014db7c634c
kai <khairulzamidi2000@gmail.com>
2025-03-15 10:38:25 +0800

    main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
+    imports: [NavbarComponent, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
     standalone: true,
 })
 export class AppComponent { }
